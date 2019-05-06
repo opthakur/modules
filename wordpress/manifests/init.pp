9d748709c403cd600a79b8645f54191cbf71fcb7
root <root@puppetworkstation.us-west1-b.c.noted-minutia-238717.internal>
2019-05-06 17:32:20 +0000

    wordpress

---
+++ wordpress/manifests/init.pp
@@ -0,0 +1,59 @@
+class wordpress {
+
+exec {'apt-get update' : 
+		path => '/usr/bin','/usr/sbin'}
+
+package {'apache2':
+		ensure => present}
+
+service {'apache2' :
+		ensure => running}
+
+$packagenames = ['mysql-server', 'mysql-client', 'php', 'libapache2-mod-php', 'php-mcrypt', 'php-mysql']
+
+$packagenames.each |String $package| {
+package {"${package}":   
+    ensure => latest,
+  }
+}
+
+exec {'mysqladmin -u root password rootpassword':
+		path => '/usr/bin'}
+
+exec {'wget https://gitlab.com/roybhaskar9/devops/raw/master/coding/chef/chefwordpress/files/default/mysqlcommands':
+		path => '/usr/bin',
+		cwd => '/tmp/'}
+
+exec {'mysql -uroot -prootpassword < /tmp/mysqlcommands':
+		path => '/usr/bin'}
+
+exec {'wget https://wordpress.org/latest.zip':
+		path => '/usr/bin',
+		cwd => '/tmp/'}
+
+package {'unzip':
+		ensure => present}
+
+exec {'sudo unzip /tmp/latest.zip -d /var/www/html':
+		path => '/usr/bin'}
+
+exec {'wget https://gitlab.com/roybhaskar9/devops/raw/master/coding/chef/chefwordpress/files/default/wp-config-sample.php':
+		path => '/usr/bin',
+		cwd => '/var/www/html/wordpress/'
+		}
+
+file {'/var/www/html/wordpress/wp-config.php':
+		ensure => present,
+		source => "/var/www/html/wordpress/wp-config-sample.php"}
+
+file {"/var/www/html/wordpress"
+		recurse => true,
+		mode => 0775,
+		owner => 'www-data',
+		group => 'www-data',
+		}
+
+exec {'service apache2 restart': 
+		path => '/usr/sbin'}
+
+}
